c252f4f46eb0b5d8001b546f4b814d369e05ed4e
ENDOH takanao <djmchl@gmail.com>
2018-01-12 10:13:42 +0700

    use make

--- json/build.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -f 1.json ]; then
-  ruby generate_json.rb
-fi
-
-nim c -o:json_nim_gcc -d:release --cc:gcc --verbosity:0 test.nim
-nim c -o:json_nim_clang -d:release --cc:clang --verbosity:0 test.nim
-go build -o json_go test.go
-gccgo -O3 -g -o json_go_gccgo test.go
--- matmul/build.sh
+++
@@ -1,4 +0,0 @@
-go build -o matmul_go matmul.go
-gccgo -O3 -g -o matmul_go_gccgo matmul.go
-nim c -o:matmul_nim_gcc --cc:gcc -d:release --verbosity:0 matmul.nim
-nim c -o:matmul_nim_clang --cc:clang -d:release --verbosity:0 matmul.nim
